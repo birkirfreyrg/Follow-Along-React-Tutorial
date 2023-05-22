b65f8b488c0cb2cf70f8f174b1cc8e4ea10f679b
birkirfreyrg <birkirfrey@msn.com>
2023-05-22 03:36:24 +0000

    My implementation of the Coding exercise. of making a button do an alert and then have a X button to remove the alert.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import ListGroup from "./components/ListGroup";
-
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
+  const [selectButton, setSelectButton] = useState(false);
+
+  const handleOnClick = () => {
+    setSelectButton(!selectButton);
   };
+
   return (
     <>
       <div>
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={handleSelectItem}
-        />
+        {selectButton === true && (
+          <>
+            <Alert>
+              My alert
+              <Button color="close" onClick={() => handleOnClick()}></Button>
+            </Alert>
+          </>
+        )}
+        <Button color="primary" onClick={() => setSelectButton(true)}>
+          My Button
+        </Button>
       </div>
     </>
   );
